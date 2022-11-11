4abf0c9e4a3fa805c4ea74f31f83e37eb03224cc
IRSAGE <sibo.egide98@gmail.com>
2022-11-11 09:10:21 +0200

    Working with routes and connecting to the backend

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,16 @@
 <template>
   <div id="app">
     <HeaderComp />
-    <TodosComp v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+    <router-view />
   </div>
 </template>
 
 <script>
-import TodosComp from "./components/Todos";
 import HeaderComp from "./components/layout/Header";
-
 export default {
-  name: "App",
+  name: "app",
   components: {
-    TodosComp,
     HeaderComp,
-  },
-  data() {
-    return {
-      todos: [
-        {
-          id: 1,
-          title: "Todo One",
-          completed: false,
-        },
-        {
-          id: 2,
-          title: "Todo Two",
-          completed: true,
-        },
-        {
-          id: 3,
-          title: "Todo Three",
-          completed: true,
-        },
-      ],
-    };
-  },
-  methods: {
-    deleteTodo(id) {
-      this.todos = this.todos.filter((todo) => todo.id !== id);
-    },
   },
 };
 </script>
@@ -53,5 +24,16 @@ export default {
 body {
   font-family: Arial, Helvetica, sans-serif;
   line-height: 1.4;
+}
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+.btn:hover {
+  background: #666;
 }
 </style>
